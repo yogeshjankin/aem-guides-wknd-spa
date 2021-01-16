c6b52ddd5d5edd504f321d316ec41c9b40adf530
yogeshjankin <yjankinkubendran@woolworths.com.au>
2021-01-17 10:08:52 +1100

    integrate-spa-changes

---
+++ ui.frontend/src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import wkndLogoDark from '../../media/wkndLogoDark.png';
+
+require('./Header.scss');
+
+export default class Header extends Component {
+
+    get logo() {
+        return (
+            <div className="Logo">
+                <img className="Logo-img" src={wkndLogoDark} alt="WKND SPA" />
+            </div>
+        );
+    }
+
+    render() {
+        return (
+                <header className="Header">
+                    <div className="Header-container">
+                        {this.logo}
+                    </div>
+                </header>
+        );
+    }
+}
